b2751ebe3fbf74a3472d94fd5bb037f44b5f4e4d
xiaoshihou <onelessononeexercise@gmail.com>
2023-03-17 20:54:00 +0800

    ma

--- update.sh
+++ update.sh
@@ -7,6 +7,7 @@ done
 cp -rf $HOME/.local/share/fonts $dot/.local/share
 cp -f $HOME/.zshrc $dot
 xbps-query -m >$dot/xbps-packages.list
+# 7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=64m -mfb=64 -ms=16g -mqs=on -sccUTF-8 -bb0 -bse0 -bsp2 '-w$HOME' -mtc=on -mta=on '$dot/themes.7z' '$HOME/.icons' '$HOME/.themes'
 # dconf reset -f /org/cinnamon/
 # dconf load /org/cinnamon/ < cinnamon.dconf
 dconf dump /org/cinnamon/ >$dot/cinnamon.dconf
